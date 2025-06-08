5702e33b2b9704b5b42b8db5594f331f2e5fceb2
Codefinity-Java <dl@codefinity.com>
2025-06-08 14:28:54 +0300

    feat: add project dependencies and structure

---
+++ src/main/java/com/auction/AuctionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.auction.pi22321;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi22321Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pi22321Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/auction/AuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.pi22321;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi22321ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
